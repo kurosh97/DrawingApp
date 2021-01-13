6e23a9f0139d3f4b5371462cbf0ca24f4839d078
kurosh97 <50096460+kurosh97@users.noreply.github.com>
2021-01-13 14:31:32 +0200

    The drawing view is set

--- app/src/main/java/com/example/drawingapp/DrawingView.kt
+++ app/src/main/java/com/example/drawingapp/DrawingView.kt
@@ -1,54 +1,99 @@
-package com.example.drawingapp
+package com.drawingapp
 
+import android.annotation.SuppressLint
 import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.os.Build
+import android.graphics.*
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
-import androidx.annotation.RequiresApi
-import java.nio.file.Path
 
-
-@RequiresApi(Build.VERSION_CODES.O)
+@SuppressLint("NewApi")
 class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) {
 
     private var mDrawPath: CustomPath? = null
     private var mCanvasBitmap: Bitmap? = null
-    private var mDrawingPaint: Paint? = null
+
+    private var mDrawPaint: Paint? = null
     private var mCanvasPaint: Paint? = null
 
-    private var mBrushSize: Float = 0.toFloat()
+    private var mBrushSize: Float =
+        0.toFloat() // A variable for stroke/brush size to draw on the canvas.
+
+
     private var color = Color.BLACK
 
-    private var mCanvas: Canvas? = null
+    private var canvas: Canvas? = null
 
     init {
         setUpDrawing()
     }
 
 
-
     private fun setUpDrawing() {
-        mDrawingPaint = Paint()
+        mDrawPaint = Paint()
         mDrawPath = CustomPath(color, mBrushSize)
-        mDrawingPaint!!.color = color
-        mDrawingPaint!!.style = Paint.Style.STROKE
-        mDrawingPaint!!.strokeJoin = Paint.Join.ROUND
-        mDrawingPaint!!.strokeCap = Paint.Cap.ROUND
-        mCanvasPaint = Paint(Paint.DITHER_FLAG)
-        mBrushSize = 20.toFloat()
 
+        mDrawPaint!!.color = color
 
+        mDrawPaint!!.style = Paint.Style.STROKE // This is to draw a STROKE style
+        mDrawPaint!!.strokeJoin = Paint.Join.ROUND // This is for store join
+        mDrawPaint!!.strokeCap = Paint.Cap.ROUND // This is for stroke Cap
+
+        mCanvasPaint = Paint(Paint.DITHER_FLAG) // Paint flag that enables dithering when blitting.
+
+        mBrushSize =
+            20.toFloat() // Here the default or we can initial brush/ stroke size is defined.
     }
 
-
-
-    internal inner class CustomPath(var color: Int, var brushThickness: Float) : Path {
-
-
+    override fun onSizeChanged(w: Int, h: Int, wprev: Int, hprev: Int) {
+        super.onSizeChanged(w, h, wprev, hprev)
+        mCanvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+        canvas = Canvas(mCanvasBitmap!!)
     }
 
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawBitmap(mCanvasBitmap!!, 0f, 0f, mCanvasPaint)
+
+        if (!mDrawPath!!.isEmpty) {
+            mDrawPaint!!.strokeWidth = mDrawPath!!.brushThickness
+            mDrawPaint!!.color = mDrawPath!!.color
+            canvas.drawPath(mDrawPath!!, mDrawPaint!!)
+        }
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        val touchX = event.x // Touch event of X coordinate
+        val touchY = event.y // touch event of Y coordinate
+
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                mDrawPath!!.color = color
+                mDrawPath!!.brushThickness = mBrushSize
+
+                mDrawPath!!.reset() // Clear any lines and curves from the path, making it empty.
+                mDrawPath!!.moveTo(
+                    touchX,
+                    touchY
+                ) // Set the beginning of the next contour to the point (x,y).
+            }
+
+            MotionEvent.ACTION_MOVE -> {
+                mDrawPath!!.lineTo(
+                    touchX,
+                    touchY
+                ) // Add a line from the last point to the specified point (x,y).
+            }
+            MotionEvent.ACTION_UP -> {
+                mDrawPath = CustomPath(color, mBrushSize)
+            }
+            else -> return false
+        }
+        invalidate()
+        return true
+    }
+
+    // An inner class for custom path with two params as color and stroke size.
+    internal inner class CustomPath(var color: Int, var brushThickness: Float) : Path()
 }
+// END
