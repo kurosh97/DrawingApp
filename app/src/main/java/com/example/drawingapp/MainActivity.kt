1488486f5b6035b72767159b7bb96ebfbc8eec73
kurosh97 <50096460+kurosh97@users.noreply.github.com>
2021-01-14 08:13:55 +0200

    added color pallets using cutom drawables

--- app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -4,17 +4,30 @@ import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageButton
+import android.widget.ImageView
+import androidx.core.content.ContextCompat
+import androidx.core.view.get
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.dialog_brush_size.*
 
 class MainActivity : AppCompatActivity() {
+    private var mImageButtonCurrentPaint: ImageView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        mImageButtonCurrentPaint = ll_paint_colors[1] as ImageButton
+        mImageButtonCurrentPaint!!.setImageDrawable(
+            ContextCompat.getDrawable(
+                this,
+                R.drawable.pellet_pressed
+            )
+        )
         drawing_view.setSizeForBrush(20.toFloat())
         ib_brush.setOnClickListener { showBrushSizeChooserDialog() }
-
     }
 
 
