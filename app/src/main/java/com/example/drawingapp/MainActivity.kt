38e6cce8609aebff465def6a14f966a8d160529d
kurosh97 <50096460+kurosh97@users.noreply.github.com>
2021-01-15 18:17:14 +0200

    added share feature

--- app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -9,6 +9,7 @@ import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
+import android.media.MediaScannerConnection
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -251,6 +252,15 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this@MainActivity, "File did not save: $result", Toast.LENGTH_LONG)
                     .show()
             }
+            MediaScannerConnection.scanFile(this@MainActivity, arrayOf(result), null) { path, uri ->
+                val shareIntent = Intent()
+                shareIntent.action = Intent.ACTION_SEND
+                shareIntent.putExtra(Intent.EXTRA_STREAM, uri)
+                shareIntent.type = "image/png"
+                startActivity(Intent.createChooser(shareIntent, "Share"))
+
+            }
+
         }
 
         private fun showProgressDialog() {
