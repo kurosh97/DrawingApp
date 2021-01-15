92a4610773680a38ee520c85da605722c818594b
kurosh97 <50096460+kurosh97@users.noreply.github.com>
2021-01-15 07:03:54 +0200

    added permission

--- app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -1,15 +1,21 @@
 package com.example.drawingapp
 
+import android.Manifest
 import android.app.Dialog
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.dialog_brush_size.*
+
 
 class MainActivity : AppCompatActivity() {
     private var mImageButtonCurrentPaint: ImageView? = null
@@ -27,6 +33,13 @@ class MainActivity : AppCompatActivity() {
         )
         drawing_view.setSizeForBrush(20.toFloat())
         ib_brush.setOnClickListener { showBrushSizeChooserDialog() }
+        ib_gallery.setOnClickListener {
+            if (isReadStorageAllowed()) {
+                //run the code to get the image from gallery
+            } else {
+                requestStoragePermission()
+            }
+        }
     }
 
 
@@ -54,13 +67,59 @@ class MainActivity : AppCompatActivity() {
         brushDialog.show()
     }
 
+    private fun requestStoragePermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                arrayOf(
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ).toString()
+            )
+        ) {
+            Toast.makeText(this, "Need permission to add a background", Toast.LENGTH_SHORT)
+                .show()
+        }
+        ActivityCompat.requestPermissions(
+            this, arrayOf(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ), STORAGE_PERMISSION_CODE
+        )
+    }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode === STORAGE_PERMISSION_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Snackbar.make(
+                    constraintlayout,
+                    "Permission Granted now you can read the storage file!",
+                    Snackbar.LENGTH_SHORT
+                )
+                    .setAction("Action", null).show()
+            }
+        } else {
+            Snackbar.make(constraintlayout, "Permission Denied!!", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
 
     fun paintClicked(view: View) {
         if (view !== mImageButtonCurrentPaint) {
             val imageButton = view as ImageButton
             val colorTag = imageButton.tag.toString()
             drawing_view.setColor(colorTag)
-            imageButton.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.pellet_pressed))
+            imageButton.setImageDrawable(
+                ContextCompat.getDrawable(
+                    this,
+                    R.drawable.pellet_pressed
+                )
+            )
 
             mImageButtonCurrentPaint!!.setImageDrawable(
                 ContextCompat.getDrawable(
@@ -71,5 +130,20 @@ class MainActivity : AppCompatActivity() {
 
             mImageButtonCurrentPaint = view
         }
+    }
+
+
+    private fun isReadStorageAllowed(): Boolean {
+        val result =
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            )
+        return result == PackageManager.PERMISSION_GRANTED
+    }
+
+
+    companion object {
+        private const val STORAGE_PERMISSION_CODE = 1
     }
 }
